8877d4a48609ce34fc7ebae7d8e26739f01a9000
“Mehran <m.kamalifard>
2023-05-28 14:02:32 +0330

    Added Day03 requirement

---
+++ AdventOfCode2022/AdventOfCode2022/Day03.swift
@@ -0,0 +1,17 @@
+//
+//  Day03.swift
+//  AdventOfCode2022
+//
+//  Created by Mehran Kamalifard on 5/28/23.
+//
+
+import Foundation
+
+struct Day03: Solution {
+    
+    var input: String
+
+    init(input: String) {
+        self.input = input
+    }
+}
