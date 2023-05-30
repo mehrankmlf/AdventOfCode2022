bf8fa9512f901b9445cf3841f268598aa20b3240
“Mehran <m.kamalifard>
2023-05-30 15:42:07 +0330

    Finished Day04 part one

--- AdventOfCode2022/AdventOfCode2022/Day04.swift
+++ AdventOfCode2022/AdventOfCode2022/Day04.swift
@@ -14,5 +14,38 @@ struct Day04: Solution {
     init(input: String) {
         self.input = input
     }
+    
+    private func isContained(left: Range<Int>, right: Range<Int>) -> Bool {
+        return left.lowerBound <= right.lowerBound && right.upperBound <= left.upperBound
+    }
+}
+
+extension Day04: Puzzels {
+    func puzzleOne() {
+        let input = self.fetchData.filter { $0 != "" }
+        var sum = 0
+        
+        for item in input {
+            let elfs = item.components(separatedBy: ",")
+            let seperatedElfs = (elf1: elfs[0].split(separator: "-"),
+                                 elf2: elfs[1].split(separator: "-"))
+            
+            let min1 = Int(seperatedElfs.elf1[0])!
+            let max1 = Int(seperatedElfs.elf1[1])!
+            
+            let min2 = Int(seperatedElfs.elf2[0])!
+            let max2 = Int(seperatedElfs.elf2[1])!
+            
+            if isContained(left: min1..<max1, right: min2..<max2) ||
+                isContained(left: min2..<max2, right: min1..<max1) {
+                sum += 1
+            }
+        }
+        print(sum)
+    }
+    
+    func puzzleTwo() {
+        print("")
+    }
 }
 
