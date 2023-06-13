be2174faf204e668f9805af4e8cc087a93f43bc6
“Mehran <m.kamalifard>
2023-06-13 12:20:27 +0330

    Added day 07 requirment

---
+++ AdventOfCode2022/AdventOfCode2022/Day07.swift
@@ -0,0 +1,17 @@
+//
+//  Day07.swift
+//  AdventOfCode2022
+//
+//  Created by Mehran Kamalifard on 6/13/23.
+//
+
+import Foundation
+
+struct Day07: Solution {
+    
+    var input: String
+    
+    init(input: String) {
+        self.input = input
+    }
+}
